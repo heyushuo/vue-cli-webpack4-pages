4abefc6e16b412a8bdaac4a5167e04434c74df43
heyushuo <heyushuo@360.cn>
2020-05-11 10:16:53 +0800

    123

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -2,16 +2,11 @@
   <div>
     <div class="home">我是home页面</div>
     <div @click="change">{{name}}</div>
-    <Commone></Commone>
   </div>
 </template>
 
 <script>
-  import Commone from './common'
   export default {
-    components: {
-      Commone
-    },
     created() {},
     mounted() {},
     data() {
